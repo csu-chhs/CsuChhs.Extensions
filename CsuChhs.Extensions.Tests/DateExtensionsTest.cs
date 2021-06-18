765db53ccb432e22eb9383abf1c4573d77b78cbc
Derek Stegelman <dstegelman@gmail.com>
2021-06-18 14:38:12 -0600

    Add tests

---
+++ CsuChhs.Extensions.Tests/DateExtensionsTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Xunit;
+
+namespace CsuChhs.Extensions.Tests
+{
+    public class DateExtensionsTest
+    {
+        [Fact]
+        public void TestMondayIsWeekday()
+        {
+            DateTime monday = DateTime.Parse("3/23/2020");
+            Assert.True(monday.IsWeekday());
+        }
+
+        [Fact]
+        public void TestSundayIsWeekend()
+        {
+            DateTime sunday = DateTime.Parse("3/29/2020");
+            Assert.False(sunday.IsWeekday());
+        }
+
+        [Fact]
+        public void TestWorkDay()
+        {
+            DateTime sunday = DateTime.Parse("08-04-2018");
+
+            Assert.False(sunday.IsWeekday());
+
+            DateTime wednesday = DateTime.Parse("08-08-2018");
+
+            Assert.True(wednesday.IsWeekday());
+        }
+    }
+}
---
+++ CsuChhs.Extensions.Tests/StringExtensionTest.cs
@@ -0,0 +1,94 @@
+﻿using Xunit;
+
+namespace CsuChhs.Extensions.Tests
+{
+    public class StringExtensionTest
+    {
+        [Fact]
+        public void TestToUrlExtension()
+        {
+            string url = "sanoteacher.weebly.com";
+            Assert.Equal("http://sanoteacher.weebly.com/", url.ToUrl());
+
+            string url2 = "http://sanoteacher.weebly.com";
+            Assert.Equal("http://sanoteacher.weebly.com/", url2.ToUrl());
+
+            string url3 = "https://sanoteacher.weebly.com";
+            Assert.Equal("https://sanoteacher.weebly.com/", url3.ToUrl());
+        }
+
+        [Fact]
+        public void TestNumericOnlyStringExtension()
+        {
+            string myString = "hello12345Iamawesome";
+            Assert.Equal("12345", myString.ToNumeric());
+        }
+
+        [Fact]
+        public void TestTruncate()
+        {
+            string longString =
+                "hello, this is a very long long long long longl ong long string, that should be truncated to 20 characters";
+            // Is 23 because we append ... to the end of the length.
+            Assert.Equal(23, longString.Truncate(20).Length);
+        }
+
+        [Fact]
+        public void TestCsvString()
+        {
+            string stringWithCommas = "string, with, some, commas, apostrophes' and double \"quotes\" ";
+            Assert.Equal("string; with; some; commas; apostrophes and double quotes ", stringWithCommas.ToCsvString());
+        }
+
+        [Fact]
+        public void TestFormattedPhoneSevenDigits()
+        {
+            Assert.Equal("123-4567", "1234567".ToPhoneNumber());
+        }
+
+        [Fact]
+        public void TestFormattedPhoneTenDigits()
+        {
+            Assert.Equal("(123) 123-1234", "1231231234".ToPhoneNumber());
+        }
+
+        [Fact]
+        public void TestFormattedPhoneSevenWithDashes()
+        {
+            Assert.Equal("123-4567", "123-4567".ToPhoneNumber());
+        }
+
+        [Fact]
+        public void TestFormattedPhoneTenWithDashes()
+        {
+            Assert.Equal("(123) 123-1234", "123-123-1234".ToPhoneNumber());
+        }
+
+        [Fact]
+        public void TestFormattedPhoneTenWithDashesParentheses()
+        {
+            Assert.Equal("(123) 123-1234", "(123)-123-1234".ToPhoneNumber());
+        }
+
+        [Fact]
+        public void TestFormattedPhoneTenWithDashesParenthesesSpaces()
+        {
+            Assert.Equal("(123) 123-1234", "(123) 123 - 1234".ToPhoneNumber());
+        }
+
+        [Fact]
+        public void TestIfNotCorrectDigitsIgnore()
+        {
+            Assert.Equal("1234", "1234".ToPhoneNumber());
+        }
+
+        [Fact]
+        public void TestFormattedPhoneElevenDigitsWithDash()
+        {
+            Assert.Equal("+1 (123) 456-7890", "1(123)456-7890".ToPhoneNumber());
+        }
+
+        
+
+    }
+}
