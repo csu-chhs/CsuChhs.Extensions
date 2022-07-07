49ddc3132bd8ed06535c341ab9b63d7a1411d4c7
Derek Stegelman <dstegelman@gmail.com>
2022-07-07 13:57:37 -0600

    Fix stack overflow

---
+++ CsuChhs.Extensions.Tests/BoolExtensionsTest.cs
@@ -0,0 +1,22 @@
+﻿using Xunit;
+
+namespace CsuChhs.Extensions.Tests
+{
+    public class BoolExtensionsTest
+    {
+        [Fact]
+        public void TestYesNoNullable()
+        {
+            bool? testingBool = false;
+
+            Assert.Equal("No", testingBool.ToYesNo());
+        }
+
+        [Fact]
+        public void TestYesNo()
+        {
+            bool testingBool = true;
+            Assert.Equal("Yes", testingBool.ToYesNo());
+        }
+    }
+}
